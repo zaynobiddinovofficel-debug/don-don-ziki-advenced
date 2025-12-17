fd8307fe8f86efd8674d3f49bd6e81f812526962
biloliddin <zaynobiddinovofficel@gmail.com>
2025-12-16 23:26:01 -1100

    Innitial commit

---
+++ html-elements.js
@@ -0,0 +1,32 @@
+// HTML elements
+const elHands = document.querySelectorAll(".js-hand");
+const elUser = document.querySelector("#user");
+const elRobot = document.querySelector("#robot");
+const elPlayAgainButton = document.querySelector("#PlayAgainButton");
+const elGameZone = document.querySelector("#gameZone");
+const elResultZone = document.querySelector("#resultZone");
+const elModeChangerButton = document.querySelector("#modeChanger");
+const elBasic = document.querySelector("#basic");
+const elAdvanced = document.querySelector("#advanced");
+const elScore = document.querySelector("#score");
+const elResultText = document.querySelector("#resultText");
+const elRulesModal = document.querySelector("#rulesModal");
+const elRulesButton = document.querySelector("#rulesButton");
+const elCloseRules = document.querySelector("#closeRules");
+
+export {
+  elHands,
+  elUser,
+  elRobot,
+  elPlayAgainButton,
+  elGameZone,
+  elResultZone,
+  elModeChangerButton,
+  elBasic,
+  elAdvanced,
+  elScore,
+  elResultText,
+  elRulesModal,
+  elRulesButton,
+  elCloseRules
+};
---
+++ js/app.js
@@ -0,0 +1,99 @@
+import {
+  elHands,
+  elUser,
+  elRobot,
+  elPlayAgainButton,
+  elGameZone,
+  elResultZone,
+  elModeChangerButton,
+  elBasic,
+  elAdvanced,
+  elScore,
+  elResultText,
+  elRulesModal,
+  elRulesButton,
+  elCloseRules
+} from "../html-elements.js";
+
+// GAME STATE
+let activeMode = "basic";
+
+// FUNCTIONS
+function swapZone(show) {
+  elGameZone.classList.toggle("hidden", show);
+  elResultZone.classList.toggle("hidden", !show);
+}
+
+function robotChoose() {
+  const basic = ["paper","scissors","rock"];
+  const advanced = ["paper","scissors","rock","scop","lizard"];
+  const hands = activeMode === "basic" ? basic : advanced;
+  return hands[Math.floor(Math.random() * hands.length)];
+}
+
+function checkWinner(user, robot) {
+  const rules = {
+    paper: { rock: "USER", scissors: "ROBOT", paper: "TIE", scop: "USER", lizard: "ROBOT" },
+    scissors: { paper: "USER", rock: "ROBOT", scissors: "TIE", lizard: "USER", scop: "ROBOT" },
+    rock: { scissors: "USER", paper: "ROBOT", rock: "TIE", lizard: "USER", scop: "ROBOT" },
+    scop: { scissors: "USER", rock: "USER", paper: "ROBOT", lizard: "ROBOT", scop: "TIE" },
+    lizard: { paper: "USER", scop: "USER", rock: "ROBOT", scissors: "ROBOT", lizard: "TIE" },
+  };
+  return rules[user][robot];
+}
+
+// HANDS CLICK
+elHands.forEach(hand => {
+  hand.addEventListener("click", evt => {
+    const user = evt.target.alt;
+    const robot = robotChoose();
+
+    elUser.src = evt.target.src;
+    elRobot.src = "./img/handload.svg";
+    swapZone(true);
+    elResultText.textContent = "";
+
+    setTimeout(() => {
+      elRobot.src = `./img/${robot}.svg`;
+
+      setTimeout(() => {
+        const winner = checkWinner(user, robot);
+        if(winner==="USER"){
+          elScore.textContent = +elScore.textContent +1;
+          elResultText.textContent = "YOU WIN";
+        } else if(winner==="ROBOT"){
+          elResultText.textContent = "YOU LOSE";
+        } else {
+          elResultText.textContent = "TIE";
+        }
+      },500);
+
+    },800);
+  });
+});
+
+// PLAY AGAIN
+elPlayAgainButton.addEventListener("click", ()=> swapZone(false));
+
+// MODE CHANGER
+elModeChangerButton.addEventListener("click", ()=>{
+  elScore.textContent="0";
+  swapZone(false);
+  if(activeMode==="basic"){
+    activeMode="advanced";
+    elBasic.classList.add("hidden");
+    elAdvanced.classList.remove("hidden");
+    elModeChangerButton.textContent="Basic";
+  } else {
+    activeMode="basic";
+    elAdvanced.classList.add("hidden");
+    elBasic.classList.remove("hidden");
+    elModeChangerButton.textContent="Advanced";
+  }
+});
+
+// RULES MODAL
+elRulesButton.addEventListener("click", ()=> elRulesModal.classList.remove("hidden"));
+elCloseRules.addEventListener("click", ()=> elRulesModal.classList.add("hidden"));
+elRulesModal.addEventListener("click", ()=> elRulesModal.classList.add("hidden"));
+// elRulesModal.querySelector(".modal-content").addEventListener("click", evt => evt.stopPropagation());
